6e86312c7fab81b6fe4b7b642961080416306b14
Anil Kumar <156671136+anildhiman093@users.noreply.github.com>
2026-01-20 03:57:38 +0530

    Create Service.php

---
+++ app/Models/Service.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'vendor_id',
+        'title',
+        'description',
+        'price',
+        'duration'
+    ];
+
+    public function vendor()
+    {
+        return $this->belongsTo(User::class, 'vendor_id');
+    }
+}
